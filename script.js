174a47a601c1aea5d6bf1a69caafc30283f137e9
camelCaseCurry <62306771+camelCaseCurry@users.noreply.github.com>
2020-05-06 16:44:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 if ($("#zip").val())==21703){
-zipAct = "main.html";}
-else {alert("We do not deliver to "+$("#zip").val());
+zipAct = "main.html";
+}
+else {
+  alert( "We do not deliver to "+ $("#zip").val() );
+  zipAct =null;
+}
+document.getElementById.('enterZip').action= "camelcasecurry.github.io/"+ zipAct;
+ 
