595b7cf41b3c7682a616786a6fb8fefbcc024786
camelCaseCurry <62306771+camelCaseCurry@users.noreply.github.com>
2020-05-06 17:28:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 if ($("#zip").val())==21703){
 zipAct = "main.html";
+document.getElementById.("enterZip").action= "https://camelcasecurry.github.io/"+ zipAct;
+ 
 }
 else {
-  alert( "We do not deliver to "+ $("#zip").val() );
+  alert( "We do not deliver to "+ $("zip").val() );
   zipAct =null;
 }
-document.getElementById.('enterZip').action= "https://camelcasecurry.github.io/"+ zipAct;
- 
+
 alert( $("#zip").val() );
